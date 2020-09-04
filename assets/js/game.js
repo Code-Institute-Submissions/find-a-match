5b9f1d5bca346d03098be08f06a75c4e0981f2ba
IvanTepes <ivan.tepes@outlook.com>
2020-09-04 16:18:20 +0000

    added game screen and positioned game indicators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-console.log("hello")
+
+
+// Main Menu New Game Button
+// 
+$('.new-game-button').click(function() {
+     $(".main-menu-screen").toggleClass("hidden");
+ });
+
+ $('.new-game-button').click(function() {
+     $(".game-screen").removeClass("hidden").addClass('visible');
+ });
