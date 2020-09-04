943e09f1bbb24e7b6a591864ca6cc8140b09c49c
IvanTepes <ivan.tepes@outlook.com>
2020-09-04 17:02:50 +0000

    added image for back of card and create card array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
 
 
-// Main Menu New Game Button
-$('.new-game-button').click(function() {
-     $(".main-menu-screen").toggleClass("hidden");
- });
 
- $('.new-game-button').click(function() {
-     $(".game-screen").removeClass("hidden").addClass('visible');
- });
+    // Cards Faces
+    const cardArray = [
+        {
+        name: 'AH',
+        img: 'assets/images/cards/AH.png'
+    },
+    {
+        name: 'AH',
+        img: 'assets/images/cards/AH.png'
+    },
+    {
+        name: 'KH',
+        img: 'assets/images/cards/KH.png'
+    },
+    {
+        name: 'KH',
+        img: 'assets/images/cards/KH.png'
+    },
+    {
+        name: 'QH',
+        img: 'assets/images/cards/QH.png'
+    },
+    {
+        name: 'QH',
+        img: 'assets/images/cards/QH.png'
+    },
+    {
+        name: 'AS',
+        img: 'assets/images/cards/AS.png'
+    },
+    {
+        name: 'AS',
+        img: 'assets/images/cards/AS.png'
+    },
+    {
+        name: 'KS',
+        img: 'assets/images/cards/KS.png'
+    },
+    {
+        name: 'KS',
+        img: 'assets/images/cards/KS.png'
+    },
+    {
+        name: 'QS',
+        img: 'assets/images/cards/QS.png'
+    },
+    {
+        name: 'QS',
+        img: 'assets/images/cards/QS.png'
+    },
+    {
+        name: 'back-of-cards',
+        img: 'assets/images/cards/back-of-cards.png'
+    },
+]
+
+
+
+//
+
+    // Main Menu New Game Button
+    $('.new-game-button').click(function() {
+        $(".main-menu-screen").toggleClass("hidden");
+    });
+
+    $('.new-game-button').click(function() {
+        $(".game-screen").removeClass("hidden").addClass('visible');
+    });
+
+})
