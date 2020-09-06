8cc925caa759813aff877a9a9d196af9df1013b6
IvanTepes <ivan.tepes@outlook.com>
2020-09-06 11:43:23 +0000

    added audio for flip and match

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,156 +1,165 @@
+//Game sounds
+class audioControl {
+    constructor() {
+        this.flipSound = new Audio('assets/audio/flip-card.mp3');
+        this.winSound = new Audio('assets/audio/win.mp3');
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    matchCard() {
+        this.winSound.play();
+    }
+}
 
-    // Cards Faces
-    // Card Array
-    const cardArray = [
-        {
-            name: 'AH',
-            img: 'assets/images/cards/AH.png'
-        },
-        {
-            name: 'AH',
-            img: 'assets/images/cards/AH.png'
-        },
-        {
-            name: 'KH',
-            img: 'assets/images/cards/KH.png'
-        },
-        {
-            name: 'KH',
-            img: 'assets/images/cards/KH.png'
-        },
-        {
-            name: 'QH',
-            img: 'assets/images/cards/QH.png'
-        },
-        {
-            name: 'QH',
-            img: 'assets/images/cards/QH.png'
-        },
-        {
-            name: 'AS',
-            img: 'assets/images/cards/AS.png'
-        },
-        {
-            name: 'AS',
-            img: 'assets/images/cards/AS.png'
-        },
-        {
-            name: 'KS',
-            img: 'assets/images/cards/KS.png'
-        },
-        {
-            name: 'KS',
-            img: 'assets/images/cards/KS.png'
-        },
-        {
-            name: 'QS',
-            img: 'assets/images/cards/QS.png'
-        },
-        {
-            name: 'QS',
-            img: 'assets/images/cards/QS.png'
-        },
-    ]
+// Cards Faces
+// Card Array
+const cardArray = [
+    {
+        name: 'AH',
+        img: 'assets/images/cards/AH.png'
+    },
+    {
+        name: 'AH',
+        img: 'assets/images/cards/AH.png'
+    },
+    {
+        name: 'KH',
+        img: 'assets/images/cards/KH.png'
+    },
+    {
+        name: 'KH',
+        img: 'assets/images/cards/KH.png'
+    },
+    {
+        name: 'QH',
+        img: 'assets/images/cards/QH.png'
+    },
+    {
+        name: 'QH',
+        img: 'assets/images/cards/QH.png'
+    },
+    {
+        name: 'AS',
+        img: 'assets/images/cards/AS.png'
+    },
+    {
+        name: 'AS',
+        img: 'assets/images/cards/AS.png'
+    },
+    {
+        name: 'KS',
+        img: 'assets/images/cards/KS.png'
+    },
+    {
+        name: 'KS',
+        img: 'assets/images/cards/KS.png'
+    },
+    {
+        name: 'QS',
+        img: 'assets/images/cards/QS.png'
+    },
+    {
+        name: 'QS',
+        img: 'assets/images/cards/QS.png'
+    },
+];
 
-    // Shuffle the cards
-    cardArray.sort(() => 0.5 - Math.random())
+// Shuffle the cards
+cardArray.sort(() => 0.5 - Math.random());
 
-    // Grid element
-    const grid = document.querySelector('.grid')
-    // Score counter and match indicator
-    let resultDisplay = document.querySelector('.score')
-    let matchDisplay = document.querySelector('.match-no-match')
-    // Chosen Cards and iDs
-    let cardsChosen = []
-    let cardsChosenId = []
-    // Array for save match cards
-    let cardsWon = []
-    console.log(cardsWon)
+// Grid element
+const grid = document.querySelector('.grid');
+// Score counter and match indicator
+let resultDisplay = document.querySelector('.score');
+let matchDisplay = document.querySelector('.match-no-match');
+// Chosen Cards and iDs
+let cardsChosen = [];
+let cardsChosenId = [];
+// Array for save match cards
+let cardsWon = [];
+// Sound from constructor
+let audio = new audioControl();
 
-    //Create Board
-    //Loop over card array for each create img element
-    //Set attribute src, id and class
-    //Append to grid
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            let card = document.createElement('img')
-            card.setAttribute('src', 'assets/images/cards/card-back.png')
-            card.setAttribute('data-id', i)
-            card.setAttribute('class', `cards`)
-            card.setAttribute('id', `cards`)
-            card.addEventListener('click', flipCard)
-            grid.appendChild(card)
-        };
-    };
+//Create Board
+//Loop over card array for each create img element
+//Set attribute src, id and class
+//Append to grid
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        let card = document.createElement('img');
+        card.setAttribute('src', 'assets/images/cards/card-back.png');
+        card.setAttribute('data-id', i);
+        card.setAttribute('class', `cards`);
+        card.setAttribute('id', `cards`);
+        card.addEventListener('click', flipCard);
+        grid.appendChild(card);
+    }
+}
 
-    // Flip Card
-    // Set cards Id
-    // Add card images
-    // If player choose two cards check for match
-    function flipCard() {
-        let cardId = this.getAttribute('data-id')
-        console.log(cardId)
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
-        };
+// Flip Card
+// Set cards Id
+// Add card images
+// If player choose two cards check for match
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    audio.flip();
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500);
+    }
+}
 
-    };
+// Check for Match Cards
+function checkForMatch() {
+    // Select all cards
+    let cards = document.querySelectorAll('img');
+    //Cards Value
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    // Check if player choose same cards
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'assets/images/cards/card-back.png');
+        cards[optionTwoId].setAttribute('src', 'assets/images/cards/card-back.png');
+        matchDisplay.textContent = 'Sorry that is same card';
+    }
+    // If Match
+    else if (cardsChosen[0] === cardsChosen[1]) {
+        // Check for cards names
+        matchDisplay.textContent = 'You found a match';
+        // If is match toggle class hidden 
+        // Cards disappear from board
+        cards[optionOneId].classList.toggle("hidden");
+        cards[optionTwoId].classList.toggle("hidden");
+        cards[optionOneId].removeEventListener('click', flipCard);
+        cards[optionTwoId].removeEventListener('click', flipCard);
+        // Push choosen cards
+        cardsWon.push(cardsChosen);
+        audio.matchCard();
+        // If not match
+        // Flip cards back and show back card image
+    } else {
+        cards[optionOneId].setAttribute('src', 'assets/images/cards/card-back.png');
+        cards[optionTwoId].setAttribute('src', 'assets/images/cards/card-back.png');
+        matchDisplay.textContent = 'Sorry No Match!';
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+        document.getElementById("end-screen").classList.remove("hidden");
+        $("#grid").empty();
+        cardsWon = [];
+    }
+}
 
-    // Check for Match Cards
-    function checkForMatch() {
-        // Select all cards
-        let cards = document.querySelectorAll('img')
-        //Cards Value
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-        console.log(cards)
-        // Check if player choose same cards
-        if (optionOneId == optionTwoId) {
-            cards[optionOneId].setAttribute('src', 'assets/images/cards/card-back.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/cards/card-back.png')
-            matchDisplay.textContent = 'Sorry that is same card'
-        }
-        // If Match
-        else if (cardsChosen[0] === cardsChosen[1]) {
-            // Check for cards names
-            matchDisplay.textContent = 'You found a match';
-            // If is match toggle class hidden 
-            // Cards disappear from board
-            cards[optionOneId].classList.toggle("hidden");
-            cards[optionTwoId].classList.toggle("hidden");
-            cards[optionOneId].removeEventListener('click', flipCard)
-            cards[optionTwoId].removeEventListener('click', flipCard)
-            // Push choosen cards
-            cardsWon.push(cardsChosen)
-            // If not match
-            // Flip cards back and show back card image
-        } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/cards/card-back.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/cards/card-back.png')
-            matchDisplay.textContent = 'Sorry No Match!'
-        }
-        cardsChosen = []
-        cardsChosenId = []
-        resultDisplay.textContent = cardsWon.length
-        if (cardsWon.length === cardArray.length / 2) {
-            document.getElementById("end-screen").classList.remove("hidden");
-            $("#grid").empty();
-        }
-    };
+createBoard();
 
+function resetGame() {
     createBoard();
- 
-   function resetGame() {
-    createBoard();
-
-};
-
-
-   
-
+}
 
 // Main Menu New Game Button
 $('.new-game-button').click(function () {
@@ -163,5 +172,5 @@ $('.new-game-button').click(function () {
 
 $('.restart-game').click(function () {
     $("#end-screen").toggleClass("hidden");
-})
+});
 
